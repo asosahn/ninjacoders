6dda9644e21a973e56561e2c9f2b5578f66e3799
Andres Sosa <ramonsosadiaz@gmail.com>
2024-01-29 02:38:10 -0600

    feature: adding apollo server

--- apis/api-auth/src/app.ts
+++ apis/api-auth/src/app.ts
@@ -1,33 +1,50 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import * as OpenApiValidator from 'express-openapi-validator';
-import {mainAllFunction} from "./router/main.router";
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const YAML = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
-const { connector } = require('swagger-routes-express');
+import express, { Application } from "express";
+import cors from "cors";
+import * as OpenApiValidator from "express-openapi-validator";
+import { mainAllFunction } from "./router/main.router";
+import { ApolloServer, BaseContext } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs } from "./graphQL/typeDefs";
+import { resolvers } from "./graphQL/resolvers";
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+const { connector } = require("swagger-routes-express");
 
 const app: Application = express();
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    morgan('dev', {
-        skip: (req: Request) => req.url === '/api/ping'
-    })
+  morgan("dev", {
+    skip: (req: Request) => req.url === "/api/ping",
+  }),
 );
-const swagger = YAML.load('./swagger/swagger.yaml');
+const swagger = YAML.load("./swagger/swagger.yaml");
 
-const connect = connector({ ...mainAllFunction }, swagger);
-const validator: any = OpenApiValidator.middleware({
-    apiSpec: './swagger/swagger.yaml',
-    validateRequests: true,
-    validateResponses: false
+const server = new ApolloServer<BaseContext>({
+  typeDefs,
+  resolvers,
 });
-app.use(validator);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
-connect(app);
+
+server.start().then(() => {
+  const connect = connector({ ...mainAllFunction }, swagger);
+  const validator: any = OpenApiValidator.middleware({
+    apiSpec: "./swagger/swagger.yaml",
+    validateRequests: true,
+    validateResponses: false,
+  });
+  app.use(validator);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server),
+  );
+  connect(app);
+});
 
 // Main Router
 // app.use("/api",mainRouter);
